7f2af14f3e3008159477bb73b5ed3cdf7d795415
29-Jude <jude77787@gmail.com>
2022-01-20 15:41:47 +0800

    Commit 8
    - Added Hazard Zones

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,11 +16,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*float HorizontalMovement = Input.GetAxis("Horizontal");
-        float VerticalMovement = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(HorizontalMovement, 0, VerticalMovement);
-        rb.AddForce(movement * Speed * Time.deltaTime);*/
-
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
